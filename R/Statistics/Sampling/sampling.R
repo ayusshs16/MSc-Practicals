46e27b2edc88e0f423576a10f3b277c99193f6d3
Ayush Srivastava <ayushsrivastava9336@gmail.com>
2026-09-14 20:38:04 +0530

    Add R sampling practical questions and solutions

---
+++ R/Statistics/Sampling/sampling.R
@@ -0,0 +1,43 @@
+# ==========================================
+# SAMPLING
+# ==========================================
+
+
+# Q1. Create a population containing numbers from 1 to 100,
+#     randomly select 10 observations, and calculate sample mean.
+
+population <- 1:100
+sampledata <- sample(population, 10)
+samplemean <- mean(sampledata)
+sampledata
+samplemean
+
+
+# Q2. Create a population from 1 to 50,
+#     randomly select 5 observations, and calculate sample mean.
+
+population <- 1:50
+sampledata <- sample(population, 5)
+samplemean <- mean(sampledata)
+sampledata
+samplemean
+
+
+# Q4. Create a population from 1 to 200,
+#     randomly select 20 observations, and calculate sample mean.
+
+population <- 1:200
+sampledata <- sample(population, 20)
+samplemean <- mean(sampledata)
+sampledata
+samplemean
+
+
+# Q5. Generate a random sample of 15 observations from 1 to 100
+#     and calculate the sample mean.
+
+population <- 1:100
+sampledata <- sample(population, 15)
+samplemean <- mean(sampledata)
+sampledata
+samplemean
